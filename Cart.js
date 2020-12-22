b8aa1a8830a1f13f488d0441209b3bc32f12ef8c
jt.jin <jt.jin@onwardsecurity.com>
2020-12-22 17:46:51 +0800

    refactoring: extract method getSize()

--- Cart.js
+++ Cart.js
@@ -10,19 +10,23 @@ class Cart {
         }
         return 100 + product.getWeight() * 10;
       case 'hsinchu':
-        const size = product.getLength() * product.getWidth() * product.getHeight();
+        const size = this.getSize(product);
         if (product.getLength() > 100 || product.getWidth() > 100 || product.getHeight() > 100) {
           return size * 0.00002 * 1100 + 500;
         }
         return size * 0.00002 * 1200;
       case 'post office':
         const feeByWeight = 80 + product.getWeight() * 10;
-        const _size = product.getLength() * product.getWidth() * product.getHeight();
+        const _size = this.getSize(product);
         const feeBySize = _size * 0.00002 * 1100;
         return Math.min(feeByWeight, feeBySize);
       default:
         throw new Error('shipper not exist');
     }
+  }
+
+  getSize(product) {
+    return product.getLength() * product.getWidth() * product.getHeight();
   }
 }
 
