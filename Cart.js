de1b5286ad415c30264ae4a596ba60ff9af2c3b9
jt.jin <jt.jin@onwardsecurity.com>
2020-12-24 12:14:35 +0800

    use HashMap instead of switch cases

--- Cart.js
+++ Cart.js
@@ -1,32 +1,21 @@
 const { BlackCat } = require('./BlackCat.js');
 const { Hsinchu } = require('./Hsinchu.js');
 const { PostOffice } = require('./PostOffice.js');
-const { Shipper } = require('./Shipper.js');
 
 class Cart {
   constructor() {
+    this.shippers = {
+      'black cat': new BlackCat(),
+      'hsinchu': new Hsinchu(),
+      'post office': new PostOffice(),
+    };
   }
 
   shippingFee(shipperName, product) {
-    return this.getShipper(shipperName).calculateFee(product);
-  }
-
-  getShipper(shipperName) {
-    let shipper = new Shipper();
-    switch (shipperName) {
-      case 'black cat':
-        shipper = new BlackCat();
-        break;
-      case 'hsinchu':
-        shipper = new Hsinchu();
-        break;
-      case 'post office':
-        shipper = new PostOffice();
-        break;
-      default:
-        throw new Error('shipper not exist');
+    if (this.shippers.hasOwnProperty(shipperName)) {
+      return this.shippers[shipperName].calculateFee(product);
     }
-    return shipper;
+    throw new Error('shipper not exist');
   }
 }
 
