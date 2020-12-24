c05476b2129527cc9b2f648ab29a60c914d2d4d3
jt.jin <jt.jin@onwardsecurity.com>
2020-12-24 12:03:22 +0800

    refactoring: move calculateFee() out of switch cases to remove duplication code

--- Cart.js
+++ Cart.js
@@ -1,22 +1,28 @@
 const { BlackCat } = require('./BlackCat.js');
 const { Hsinchu } = require('./Hsinchu.js');
 const { PostOffice } = require('./PostOffice.js');
+const { Shipper } = require('./Shipper.js');
 
 class Cart {
   constructor() {
   }
 
   shippingFee(shipperName, product) {
+    let shipper = new Shipper();
     switch (shipperName) {
       case 'black cat':
-        return new BlackCat().calculateFee(product);
+        shipper = new BlackCat();
+        break;
       case 'hsinchu':
-        return new Hsinchu().calculateFee(product);
+        shipper = new Hsinchu();
+        break;
       case 'post office':
-        return new PostOffice().calculateFee(product);
+        shipper = new PostOffice();
+        break;
       default:
         throw new Error('shipper not exist');
     }
+    return shipper.calculateFee(product);
   }
 }
 
